24e9991f7d1d9718c110d4b4c34750dc3b24695d
josenel <josenel.jt@gmail.com>
2022-08-29 20:35:38 -0500

    Se añade diseño responsivo

--- script.js
+++ script.js
@@ -10,11 +10,10 @@ let CajaTextoResult = document.querySelector("#areaCopia");
 let botEncrip = document.querySelector("#botEncrip");
 let botDesencrip = document.querySelector("#botDesencrip");
 let botCopy = document.querySelector("#botCopiar");
-let textoInfo = document.querySelector("#textoInicial");
 
 //reglas de caracteres permitidos
-const letrasOk = new RegExp("^[a-z]+$");
-const espacio = new RegExp(/ /, "g");
+const letrasOk = new RegExp("^[a-z]+$","i");  //[a-z]solo letras, +$ se puede repitir y i se permite mayusculas
+const espacio = new RegExp(/ /, "g");//se permiten espacios
 
 //limpia textarea o los input
 function limpiarTexto (cajaTexto){
@@ -22,60 +21,71 @@ function limpiarTexto (cajaTexto){
 }
 
 function encriptador (){
-    limpiarTexto(CajaTextoResult);
-    ocultarElementos("muneco");
-    mostrarElementos("botCopiar");
-    mostrarElementos("areaCopia");
-    ocultarElementos("letra1");
-    ocultarElementos("letra2");
-    let contador = 1;
-    let caracterEncriptado = "";
-    let frase = cajaTexto1.value;         
-    let largoFrase = frase.length;
-    let caracter = "";
-    textoEncriptado ="";
+    if(cajaTexto1.value==""){
 
-    //compara cada caracter o letra y se va alimentando "textoEncriptado", dependiendo de la condicion if
-    while( contador <= largoFrase ){
-        caracter = frase.charAt(contador -1);
-        let igual = true;
-        for (let posicion = 0; posicion < letraNormal.length; posicion++){
-            if(caracter == letraNormal[posicion]){
-                caracterEncriptado = letraEncrip[posicion];
-                igual = true;
-                break;
-            }else{
-                igual = false;
+    }else{
+        limpiarTexto(CajaTextoResult);
+        mostrarElementos("botCopiar");
+        mostrarElementos("areaCopia");
+        ocultarElementos("letra1");
+        ocultarElementos("letra2");
+        let contador = 1;
+        let caracterEncriptado = "";
+        let frase = cajaTexto1.value;         
+        let largoFrase = frase.length;
+        let caracter = "";
+        textoEncriptado ="";
+
+        //compara cada caracter o letra y se va alimentando "textoEncriptado", dependiendo de la condicion if
+        while( contador <= largoFrase ){
+            caracter = frase.charAt(contador -1);
+            let igual = true;
+            for (let posicion = 0; posicion < letraNormal.length; posicion++){
+                if(caracter == letraNormal[posicion]){
+                    caracterEncriptado = letraEncrip[posicion];
+                    igual = true;
+                    break;
+                }else{
+                    igual = false;
+                }
             }
+            if (igual == false){
+                caracterEncriptado = caracter;
+            }
+            textoEncriptado = textoEncriptado + caracterEncriptado;
+            CajaTextoResult.value = textoEncriptado;
+            contador++;
         }
-        if (igual == false){
-            caracterEncriptado = caracter;
+        CajaTextoResult.style.background = "white";
         }
-        textoEncriptado = textoEncriptado + caracterEncriptado;
-        CajaTextoResult.value = textoEncriptado;
-        contador++;
-    }
+    
 }
 
 
 function desencriptador(){
-    //funciones visuales
-    limpiarTexto(CajaTextoResult);
-    ocultarElementos("muneco");
-    mostrarElementos("botCopiar");
-    mostrarElementos("areaCopia");
-    ocultarElementos("letra1");
-    ocultarElementos("letra2");
 
-    //compara toda la frase en busqueda de los caracteres encriptados, y lo reemplaza con la letra correspondiente (vocal)
-    let contador = 0;
-    let frase = cajaTexto1.value;         
-    while( contador < letraEncrip.length ){ 
-        textoDesencriptado = frase.replace(new RegExp(letraEncrip[contador], "g"), letraNormal[contador]);
-        frase = textoDesencriptado;
-        CajaTextoResult.value = textoDesencriptado;
-        contador++;  
-    }           
+    if(cajaTexto1.value==""){
+
+    }else{
+        //funciones visuales
+        limpiarTexto(CajaTextoResult);
+        mostrarElementos("botCopiar");
+        mostrarElementos("areaCopia");
+        ocultarElementos("letra1");
+        ocultarElementos("letra2");
+
+        //compara toda la frase en busqueda de los caracteres encriptados, y lo reemplaza con la letra correspondiente (vocal)
+        let contador = 0;
+        let frase = cajaTexto1.value;         
+        while( contador < letraEncrip.length ){ 
+            textoDesencriptado = frase.replace(new RegExp(letraEncrip[contador], "g"), letraNormal[contador]);
+            frase = textoDesencriptado;
+            CajaTextoResult.value = textoDesencriptado;
+            contador++;  
+        }
+        CajaTextoResult.style.background = "white"; 
+    }
+              
 }
 
 // evita que se ingresen catacteres fuera de las 2 reglas declaradas
@@ -102,16 +112,8 @@ function mostrarElementos(idElemento){
     document.getElementById(idElemento).style.visibility = "visible";
 }
 
-//oculta texto al hacer click
-function ocultarConClick(){
-    document.getElementById("textoInicial").style.visibility = "hidden";
-    document.getElementById("text1").focus();
-}
-
 //parametros iniciales al cargar pagina
-ocultarElementos("areaCopia");
 ocultarElementos("botCopiar");
-textoInfo.onclick = ocultarConClick;
 cajaTexto1.onkeypress = validarTexto;
 botDesencrip.onclick = desencriptador;
 botEncrip.onclick = encriptador;
